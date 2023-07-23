501c83b4e43530e3b5921c143bfbe443e755abdf
Ridwan Hidayat <ridwan.nurulhidayat@gmail.com>
2023-07-24 06:55:10 +0700

    adding billboard

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,18 +1,32 @@
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, auth } from "@clerk/nextjs";
 import { MainNav } from "@/components/main-nav";
+import StoreSwitcher from "@/components/store-swithcer";
+import { redirect } from "next/navigation";
+import prismadb from "@/lib/prismadb";
 
-const Navbar = () => {
-    return (
-        <div className="border-b">
-            <div className="flex h-16 items-center px-4">
-                <div>This will be a store swticher</div>
-                <MainNav />
-                <div className="ml-auto flex items-center space-x-4">
-                    <UserButton afterSignOutUrl="/" />
-                </div>
-            </div>
+const Navbar = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  const stores = await prismadb.store.findMany({
+    where: {
+      userId,
+    },
+  });
+  return (
+    <div className="border-b">
+      <div className="flex h-16 items-center px-4">
+        <StoreSwitcher items={stores} />
+        <MainNav className="mx-6" />
+        <div className="ml-auto flex items-center space-x-4">
+          <UserButton afterSignOutUrl="/" />
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
